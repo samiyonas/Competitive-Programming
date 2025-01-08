8f3b1e2e6feef7a215cc93c0e59fcb70c9abad9b
samiyonas <samuel.x.yonas@gmail.com>
2025-01-08 18:03:21 +0300

    New algorithm

---
+++ fizzbuzz.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+def fizzbuzz(n):
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+def main():
+    fizzbuzz(100)
+
+if __name__ == "__main__":
+    def main()
